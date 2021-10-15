d1e6ffb602fe45f9ad49f6f4f89e3a4c03851114
Hans Holmberg <hans.holmberg@wdc.com>
2021-10-15 09:52:53 +0000

    tests: improve fillrandom_sync test

--- tests/staging/0002_fillrandom_sync.sh
+++ tests/staging/0002_fillrandom_sync.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-WORKLOAD_SZ=10000000000
+WORKLOAD_SZ=1000000000
 
 echo "" > $TEST_OUT
 
-for VALUE_SIZE in 2000 8000; do
+for VALUE_SIZE in 1000 2000 4000; do
   NUM=$(( $WORKLOAD_SZ / $VALUE_SIZE ))
   DB_BENCH_PARAMS="--benchmarks=fillrandom --num=$NUM --value_size=$VALUE_SIZE --compression_type=none --sync=1 --histogram $FS_PARAMS $DB_BENCH_EXTRA_PARAMS"
   echo "# Running db_bench with parameters: $DB_BENCH_PARAMS" >> $TEST_OUT
