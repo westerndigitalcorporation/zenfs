e580b77a99e20ba0755fe4367832470c61554ddf
Andreas Hindborg <andreas.hindborg@wdc.com>
2021-11-22 09:39:59 +0000

    build: Use pkg-config to specify dependencies
    This change requires rocksdb >= 6.29.
    
    Signed-off-by: Andreas Hindborg <andreas.hindborg@wdc.com>

--- zenfs.mk
+++ zenfs.mk
@@ -1,6 +1,6 @@
 zenfs_SOURCES = fs/fs_zenfs.cc fs/zbd_zenfs.cc fs/io_zenfs.cc
 zenfs_HEADERS = fs/fs_zenfs.h fs/zbd_zenfs.h fs/io_zenfs.h fs/version.h fs/metrics.h fs/snapshot.h
-zenfs_LDFLAGS = -lzbd -u zenfs_filesystem_reg
+zenfs_LDFLAGS = -u zenfs_filesystem_reg
 
 ZENFS_ROOT_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 
@@ -8,3 +8,5 @@ $(shell cd $(ZENFS_ROOT_DIR) && ./generate-version.sh)
 ifneq ($(.SHELLSTATUS),0)
 $(error Generating ZenFS version failed)
 endif
+
+zenfs_PKGCONFIG_REQUIRES = "libzbd >= 1.5.0"
