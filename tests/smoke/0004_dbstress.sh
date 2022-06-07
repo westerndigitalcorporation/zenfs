0bf39161a956fb5368b38f49de9c2c7769c0504a
Andreas Hindborg <andreas.hindborg@wdc.com>
2022-06-07 13:49:53 +0200

    test: Explicitly disable read fault injection for db_stress
    Signed-off-by: Andreas Hindborg <andreas.hindborg@wdc.com>

--- tests/smoke/0004_dbstress.sh
+++ tests/smoke/0004_dbstress.sh
@@ -10,7 +10,7 @@ fi
 OPS_PER_THREAD=1000
 REOPENS=5
 
-DB_STRESS_PARAMS="--ops_per_thread=$OPS_PER_THREAD --reopen=$REOPENS --db=dbname $FS_PARAMS"
+DB_STRESS_PARAMS="--ops_per_thread=$OPS_PER_THREAD --reopen=$REOPENS --db=dbname $FS_PARAMS --read_fault_one_in=0"
 
 echo "# Running db_stress with parameters: $DB_STRESS_PARAMS" > $TEST_OUT
 $TOOLS_DIR/db_stress $DB_STRESS_PARAMS >> $TEST_OUT
